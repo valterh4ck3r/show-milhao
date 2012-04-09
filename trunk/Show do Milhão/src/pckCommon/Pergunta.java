d79353afa50bb3066381bab256a9a9cbe988129f
gustavotogni@gmail.com <gustavotogni@gmail.com@906ed89c-2055-42f0-ab96-fd06bed64179>
2012-04-09 13:43:34 +0000

    Adicionadas 100 perguntas. Pequenas mudanças em certas classes. Iniciada classe de processamento de perguntas.

--- trunk/Show do Milhão/src/pckCommon/Pergunta.java
+++ trunk/Show do Milhão/src/pckCommon/Pergunta.java
@@ -1,47 +1,69 @@
 package pckCommon;
 
-public class Pergunta {
+public class Pergunta
+{
 	int id;
 	String descricao;
 	NivelPergunta nivel;
 	OpcaoPergunta[] opcoes; //array de opcoes
+	int resposta;
 
-	public Pergunta(int id, String descricao, OpcaoPergunta[] opcoes, NivelPergunta nivel){
-		this.id = id;
-		this.descricao = descricao;
-		this.opcoes = opcoes;
-		this.nivel = nivel;
+	public Pergunta(int id, String descricao, OpcaoPergunta[] opcoes, int resposta, NivelPergunta nivel)
+	{
+		this.id			= id;
+		this.descricao	= descricao;
+		this.opcoes		= opcoes;
+		this.resposta	= resposta;
+		this.nivel		= nivel;
 	}
 
-	public int getId() {
+	public int getResposta()
+	{
+		return resposta;
+	}
+
+	public void setResposta(int resposta)
+	{
+		this.resposta = resposta;
+	}
+
+	public int getId()
+	{
 		return id;
 	}
 
-	public void setId(int id) {
+	public void setId(int id)
+	{
 		this.id = id;
 	}
 
-	public String getDescricao() {
+	public String getDescricao()
+	{
 		return descricao;
 	}
 
-	public void setDescricao(String descricao) {
+	public void setDescricao(String descricao)
+	{
 		this.descricao = descricao;
 	}
 
-	public NivelPergunta getNivel() {
+	public NivelPergunta getNivel()
+	{
 		return nivel;
 	}
 
-	public void setNivel(NivelPergunta nivel) {
+	public void setNivel(NivelPergunta nivel)
+	{
 		this.nivel = nivel;
 	}
 
-	public OpcaoPergunta[] getOpcoes() {
+	public OpcaoPergunta[] getOpcoes()
+	{
 		return opcoes;
 	}
 
-	public void setOpcoes(OpcaoPergunta[] opcoes) {
+	public void setOpcoes(OpcaoPergunta[] opcoes)
+	{
 		this.opcoes = opcoes;
 	}
 }
