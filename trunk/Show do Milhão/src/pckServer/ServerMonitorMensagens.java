cfbf3dd2df8927c6afedd7645c64b998bf1aec7a
gustavotogni@gmail.com <gustavotogni@gmail.com@906ed89c-2055-42f0-ab96-fd06bed64179>
2012-05-02 16:14:42 +0000

    Mais limpezas.

--- trunk/Show do Milhão/src/pckServer/ServerMonitorMensagens.java
+++ trunk/Show do Milhão/src/pckServer/ServerMonitorMensagens.java
@@ -6,38 +6,39 @@ import java.net.Socket;
 
 import javax.swing.JOptionPane;
 
-import pckClient.Cliente;
 import pckCommon.Mensagem;
 
-public class ServerMonitorMensagens implements Runnable {
-	
+public class ServerMonitorMensagens implements Runnable
+{
 	private Servidor serv;
 	private ServerSocket socketRecepcao;
 	private Socket socketConexao;
 	private ObjectInputStream input;
 	
-	public ServerMonitorMensagens(Servidor serv){
+	public ServerMonitorMensagens(Servidor serv)
+	{
 		this.serv = serv;
-		try{
+		
+		try
+		{
 			this.socketRecepcao = new ServerSocket(6789);
 			this.socketConexao = socketRecepcao.accept();
 			this.input = new ObjectInputStream(socketConexao.getInputStream());
 		}
-		catch(Exception e){
-			JOptionPane.showMessageDialog(null, e, "Erro", JOptionPane.WARNING_MESSAGE);							
-		}
+		
+		catch(Exception e){ JOptionPane.showMessageDialog(null, e, "Erro", JOptionPane.WARNING_MESSAGE); }
 	}
 	
-	public void run() {
-		// TODO Auto-generated method stub
+	public void run()
+	{
 		Mensagem msg;
-		try{
-		  while(true){
+		
+		try
+		{
+		  while(true)
 			  msg  = (Mensagem)input.readObject();
-		  }
 		}
-		catch(Exception e){
-			JOptionPane.showMessageDialog(null, e, "Erro", JOptionPane.WARNING_MESSAGE);							
-		}
+		
+		catch(Exception e){ JOptionPane.showMessageDialog(null, e, "Erro", JOptionPane.WARNING_MESSAGE); }
 	}	
 }
